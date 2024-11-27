fb8fc294ab70928b27892139ff6a184edd937de3
NOVIYANTOKAMALUDIN <noviyantokamaludin@gmail.com>
2024-11-27 17:13:16 +0700

    tes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Task Manager</h1>
+        <h1 className="text-xl font-bold">Tasker</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
